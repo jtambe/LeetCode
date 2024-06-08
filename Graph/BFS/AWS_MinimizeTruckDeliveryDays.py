9e2fb7776f2ab7d8fdec7942d5f260e75ce16ee8
jtambe <tambe_jayesh@hotmail.com>
2024-06-08 15:49:30 -0700

    2 AWS problems from web

---
+++ Graph/BFS/AWS_MinimizeTruckDeliveryDays.py
@@ -0,0 +1,94 @@
+"""
+
+A new e-commerce website is launched, they have warehouses setup in a couple of locations across the world. (mxn world representation).
+Currently they have only procured trucks for delivery, which travel only to adjacent countries by road (vertical and horizontal). 
+There are no roads on water. An average truck takes 20 days to travel between two countries. 
+Find the least number of days taken to deliver to the entire world, if at all.
+
+Input - worldMap : 2D Array
+* C : country
+  - O - No warehouse
+  - X - Has warehouse
+* W - water
+
+For Reference - World Map
+[C W C W C C C
+ W C W C C W C
+ W W C C W W C
+ W C C C W W C
+ C C W C W C C]
+
+ 
+[X1 W O2]
+1-> 0 days
+2 -> 20 days (1-> 2 : 20 )
+ 
+
+
+Actual Input -
+[X O X W O X O
+ W O W O X W O
+ W W N O W W O
+ W O O O W W O
+ X X W O W O X]
+
+
+ Interpretation:
+ X are locations that have trucks
+ O are location that don't have trucks
+ W is water and trucks can't go over water
+ Starting from all X can trucks deliver to all O locations?
+ If they can, how many days will it take? 
+ Moving to O location from X or from O takes 20 days
+""" 
+
+
+from typing import List, Deque
+
+class Solution:
+    def deliveryCost(self, map: List[List[str]]) -> int:
+        
+        q = Deque()
+        rows, cols = len(map), len(map[0])
+        days = 0
+        countCountries = 0
+        dirs = [(0,+1), (0,-1),(+1,0),(-1,0)]
+        
+        def inbound(x:int, y:int) -> bool:
+            if x >= 0 and x < rows and y >= 0 and y < cols:
+                return True
+            else:
+                return False
+        
+        # starting positions for BFS
+        for x in range(rows):
+            for y in range(cols):
+                if map[x][y] == "X":
+                    q.append((x,y))
+                    countCountries += 1
+                if map[x][y] == "O":
+                    countCountries += 1
+                    
+        
+        visited = set()
+        while(q):
+            for i in range(len(q)):
+                cellX, cellY = q.popleft()
+                visited.add(cellX, cellY)                
+                countCountries -= 1
+                for dx,dy in dirs:
+                    x,y = cellX+dx, cellY+dy
+                    if inbound(x,y) and map[x][y] == "O":
+                        if (x,y) not in visited:
+                            visited.add(cellX, cellY)
+                            q.append((x,y))
+                            countCountries -= 1
+            if len(q) > 0:                            
+                days += 20
+                    
+        
+        return days if countCountries == 0 else -1
+    
+
+
+    
---
+++ Graph/DFS/AWS_EmployeeReferralImpact.py
@@ -0,0 +1,82 @@
+from typing import List, Dict
+"""
+Given a list of employees and their referrals, count the referralImpact for each employee
+No cycles exist in adjecency list
+
+Input : [[1,2][2,3][3,4][2,4][4,5]]
+Output:
+1: 4
+2: 3
+3: 2
+4: 1
+5: 0
+"""
+class solution:
+
+    def referralImpact(self, referrals : List[List[int]]) -> Dict[int, int]:
+
+        d = {} # emp :(referrals)
+        ans = {} # emp : referralImpact
+
+        # create adjencency List
+        # create ans dictionary marked 0 for all employees
+        for x in referrals:
+            emp = x[0]
+            referred = x[1]
+            if emp in d:
+                alist = d[emp]
+                alist.add(referred)
+                d[emp] = alist
+            else:
+                d[emp] = set()
+                d[emp].add(referred)
+            if referred not in d:
+                d[referred] = set()
+            ans[emp] = 0
+            ans[referred] = 0
+
+        visitedEmp = set()
+        def dfs(emp: int, parents: List[int], visitedReferred: set):
+            
+            # if there are no further employees to explore return
+            if not d[emp]: 
+                return
+            for referral in d[emp]:
+                if referral not in visitedReferred:
+                    ans[emp] = ans[emp] +1
+                    for x in parents:
+                        ans[x] = ans[x] + 1
+                    visitedReferred.add(referral)
+
+                if referral not in visitedEmp:
+                    visitedEmp.add(referral)
+                    parents.append(emp)
+                    dfs(referral, parents, visitedReferred)
+                    # once chain ends and dfs returns from base condition, 
+                    # before we explore next referral in adjecency list, we need to remove last added parent from stack
+                    # This will avoid repeating that parent in subsequent dfs stack
+                    parents.pop()
+
+        for emp in d:
+            if emp not in visitedEmp:
+                visitedEmp.add(emp)
+                dfs(emp, [], set())
+        return ans
+
+
+sln = solution()
+referrals = [[1,2],[1,3],[1,4],[1,5],[2,3],[2,6],[6,8]]
+# d {1: {2, 3, 4, 5}, 2: {3, 6}, 3: set(), 4: set(), 5: set(), 6: {8}, 8: set()}
+# expected output : {1:6, 2:3, 3:0, 4: 0, 5:0, 6:2, 8: 0}
+ans = sln.referralImpact(referrals)
+print(ans)
+
+referrals = [[1,2],[2,3],[3,4],[2,4],[4,5]]
+# {1: 4, 2: 3, 3: 2, 4: 1, 5: 0}
+ans = sln.referralImpact(referrals)
+print(ans)
+
+referrals = [[1,2],[9,3]]
+ans = sln.referralImpact(referrals)
+print(ans)
+
