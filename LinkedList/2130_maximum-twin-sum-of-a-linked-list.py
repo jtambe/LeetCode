4e1b8fd6dea412c33ae29681eedc25248475cbdc
Jayesh Tambe <tambe_jayesh@hotmail.com>
2025-10-12 10:23:00 -0700

    Implement alternative solution for maximum twin sum
    Added a new solution class with methods to calculate the maximum twin sum of a linked list using a reversed list approach.

--- LinkedList/2130_maximum-twin-sum-of-a-linked-list.py
+++ LinkedList/2130_maximum-twin-sum-of-a-linked-list.py
@@ -26,3 +26,42 @@ class Solution:
             r -= 1
 
         return maxSum
+
+
+
+class Solution2:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        n = self.get_count(head)
+        reversed_head = self.get_reverse_list_head(head, n)
+        i = 0
+        max_sum = 0
+        cur, rev_cur = head, reversed_head
+        while(i < n//2):
+            max_sum = max(max_sum , cur.val + rev_cur.val)
+            cur, rev_cur = cur.next, rev_cur.next
+            i += 1
+        return max_sum
+
+    def get_reverse_list_head(self, head: Optional[ListNode], n:int) -> ListNode:
+        i = 0
+        cur = head
+        while(i < n//2):
+            cur = cur.next
+            i += 1
+
+        prev = None
+        while(cur):
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        return prev
+
+    def get_count(self, head: Optional[ListNode]) -> int:
+        count = 0
+        cur = head
+        while(cur):
+            cur = cur.next
+            count += 1
+        return count
+
