7ae1b80262c621d9ea12ae01ef9bfa24a88fb66d
jtambe <tambe_jayesh@hotmail.com>
2026-03-25 22:44:22 -0700

    optimized LOC in 58 string

--- String/58_length-of-last-word.py
+++ String/58_length-of-last-word.py
@@ -6,31 +6,33 @@ A word is a maximal substring consisting of non-space characters only.
  
 
 Example 1:
-
 Input: s = "Hello World"
 Output: 5
 Explanation: The last word is "World" with length 5.
 Example 2:
-
 Input: s = "   fly me   to   the moon  "
 Output: 4
 Explanation: The last word is "moon" with length 4.
 Example 3:
-
 Input: s = "luffy is still joyboy"
 Output: 6
 Explanation: The last word is "joyboy" with length 6.
  
 
 Constraints:
-
-1 <= s.length <= 104
+1 <= s.length <= 10^4
 s consists of only English letters and spaces ' '.
 There will be at least one word in s.
 """
 
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
+
+        s = s.rstrip()
+        s_arr = s.split(" ")
+        return len(s_arr[-1])
+    
+    def lengthOfLastWord_2(self, s: str) -> int:
 
         r = len(s)-1
         last_word_length = 0
